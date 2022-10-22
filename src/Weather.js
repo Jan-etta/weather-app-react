3c8f5002a85cce9414482b55340ed1ef6dd5832a
Jan-etta <Jandanvers1@gmail.com>
2022-10-22 15:23:30 +0100

    import bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -3,12 +3,20 @@ import React from "react";
 import Search from "./Search";
 export default function Weather() {
   return (
-    <div className="container-fluid">
+    <div className="Weather container">
       <h1>What's the Weather</h1>
-
-      <Search />
+      <div className="row">
+        <div className="col-8">
+          <Search />
+        </div></div>
+      
       <br />
-      <a target="_blank" rel="noreferrer" href="https://github.com/Jan-etta/weather-app-react">Coded by Jan-etta </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/Jan-etta/weather-app-react">
+        Coded by Jan-etta{" "}
+      </a>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import "bootstrap/dist/css/bootstrap.css";
 import Weather from "./Weather";
 
 const rootElement = document.getElementById("root");
@@ -9,5 +9,11 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <Weather />
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://github.com/Jan-etta/weather-app-react">
+      Coded by Jan-etta{" "}
+    </a>
   </StrictMode>
 );
