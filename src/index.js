ac4001f099d2e074ec668e03f8197428ef729c93
Jan-etta <Jandanvers1@gmail.com>
2022-10-27 18:07:13 +0100

    Credit

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import Weather from "./Weather";
-
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Weather />
-    <a
-      target="_blank"
-      rel="noreferrer"
-      href="https://github.com/Jan-etta/weather-app-react">
-      Coded by Jan-etta{" "}
-    </a>
+    <div className="container">
+      <div className="Weather">
+        <Weather />
+        
+      </div>
+      <div>
+        Opensource code by{" "}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/Jan-etta/weather-app-react">
+          Jan-etta
+        </a>
+      </div>
+    </div>
   </StrictMode>
 );
